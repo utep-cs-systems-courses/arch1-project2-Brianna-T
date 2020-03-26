472b76e6454aa0895ed6a7ff152bb66e03560966
Brianna T <btovar@miners.utep.edu>
2020-03-26 15:39:39 -0600

    Fixed

--- project/Toy/Blinkmain.c
+++ project/Toy/Blinkmain.c
@@ -1,7 +1,11 @@
-#include <Timer.c>
+#include <msp430.h>
+#include "Timer.c"
+#include "Blinkled.c"
 
-int main():{
+int main(void){
   configureClocks();
   led();
-  update();
+  interrupts(); /*timer interrupt*/
+
+  or_sr(0x18); /*CPU off, GIE on, power save*/
 }
