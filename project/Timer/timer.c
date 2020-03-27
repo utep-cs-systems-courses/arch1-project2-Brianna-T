7695e0356a6beebc6e44d86fce80220ba5340682
Brianna T <btovar@miners.utep.edu>
2020-03-27 15:56:00 -0600

    Final

--- project/Timer/timer.c
+++ project/Timer/timer.c
@@ -3,17 +3,25 @@
 
 void configureClocks(){
   WDTCTL = WDTPW + WDTHOLD; //Disable Watchdog Timer
-  BCSTL1 = CALBC1_10MHZ;//my modification is 10 mhz
-  DC0CTL = CALDC0_10MHZ;
+  BCSCTL1 = CALBC1_1MHZ;//my modification is 1 mhz
+  DCOCTL = CALDCO_1MHZ;
 
   BCSCTL2 &= ~(SELS); //source = DCO
   BCSCTL2 |= DIVS_2;//modify DCO/4
 }
 
-void enableWDTInterrupts():{
+void interrupts(){
   WDTCTL = WDTPW | //device reset
   WDTTMSEL |
   WDTCNTCL |
-    1;
+    1;//divide SMCLK by 8192.. why?
   IE1 |= WDTIE;
+}
+
+void timerAUmode(){
+  TA0CCR0 =0;
+  TA0CCR1 =0;
+  TA0CCTL1 = OUTMOD_3; /* Toggle p1.6 timer=1*/
+
+  TACTL = TASSEL_2 +MC_1; /*Timer control, system clock (SMCLK)*/
 }
