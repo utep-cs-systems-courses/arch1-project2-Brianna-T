6f5df77e723d60720be8ad51723614f5a2e88f50
Brianna T <btovar@miners.utep.edu>
2020-04-14 23:00:09 -0600

    FixedFinal3

--- project/Toy/buzz.c
+++ project/Toy/buzz.c
@@ -1,6 +1,8 @@
 #include <msp430.h>
+#include "libTimer.h"
+#include "buzz.h"
 
-void buzzer()
+void buzzer_init()
 {
     /* P2SEL2.6, P2SEL2.7, anmd P2SEL.7 must be zero */
     timerAUpmode(); /* used to drive speaker */
@@ -10,10 +12,23 @@ void buzzer()
     P2DIR = BIT6; /*output to speaker(P2.6) */
 }
 
-void start(short cycles) /* buzzer clock = 2MHz(period of 1k results in 2kHz tone) */
+char buzzer_set_period(short cycles) /* buzzer clock = 2MHz(period of 1k results in 2kHz tone) */
 {
   CCR0 = cycles; 
   CCR1 = cycles >> 1;/* one half cycle */
+}
+
+void loop(){
+  short tone = buzzer_set_period(10000);
+
+  for(int i=0; i<2;i++){
+    if(i=1){
+      tone= buzzer_set_period(1000);
+    }
+    if(i=2){
+      tone= buzzer_set_period(10000);
+    }
+  }
 }
 
 
