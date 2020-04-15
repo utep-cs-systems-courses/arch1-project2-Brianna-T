6fe1cce3d7a23c439d2f76b88b0e23d73d57bba0
Brianna T <btovar@miners.utep.edu>
2020-04-14 21:21:48 -0600

    Final3

--- project/Toy/Blinkmain.c
+++ project/Toy/Blinkmain.c
@@ -1,11 +1,11 @@
 #include <msp430.h>
-#include "timer.h"
-#include "Blinkled.c"
+#include "libTimer.h"
+#include "Blinkled.h"
 
 int main(void){
   configureClocks();
-  led();
-  interrupts(); /*timer interrupt*/
+  led_init();
+  enableWDTInterrupts(); /*timer interrupt*/
 
   or_sr(0x18); /*CPU off, GIE on, power save*/
 }
